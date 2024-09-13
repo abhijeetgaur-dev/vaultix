32b15cea456110509be7400153fcea8c238272bb
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2024-09-13 20:43:31 +0530

    added images

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -82,8 +82,8 @@ const Sidebar = () => {
         }`}
       >
         <Image
-          src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
-          alt="edstock-logo"
+          src="https://s3-vaultixmanagement.s3.ap-south-1.amazonaws.com/logo.png"
+          alt="vaultix-logo"
           width={27}
           height={27}
           className="rounded w-8"
